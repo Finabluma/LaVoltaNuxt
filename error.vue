8adb4332f7cde721f1d31bff827aba5da70c6f69
finabluma <oscar.francino@finabluma.com>
2025-01-20 20:53:19 +0100

    botero error

--- error.vue
+++ error.vue
@@ -8,19 +8,20 @@
   <div class="relative bg-slate-200 dark:bg-slate-600">
     <AppHeader />
     <main>
-      <div
-        class="w-full h-full flex flex-col justify-center items-center py-20"
-      >
-        <div class="content text-center">
-          <h1 class="statusCode">
+      <div class="wrapper relative max-w-screen-2xl flex justify-center">
+        <div class="content">
+          <h1 class="statusCode sr-only">
             {{ error.statusCode }}
           </h1>
-          <NuxtLink to="/" class="font-typewriter text-xl">
-            <span class="text-lg block">Algo salió mal</span> Vuelta a Inicio 🙏
+          <NuxtLink
+            to="/"
+            class="font-cameo text-slate-800 dark:text-slate-400 absolute bottom-20 xl:bottom-0 right-auto w-full h-[10vh] left-auto flex justify-center"
+          >
+            <span class="block mr-2">Algo salió mal </span> Vuelve al Inicio
           </NuxtLink>
         </div>
-        <div class="img w-[50vw] md:w-[40vw] h-full mx-auto">
-          <img src="./assets/images/botero.svg" alt="" />
+        <div class="svg overflow-hidden">
+          <SVGAppBotero />
         </div>
       </div>
     </main>
@@ -28,7 +29,7 @@
   </div>
 </template>
 <style lang="postcss" scoped>
-  .statusCode {
-    @apply font-cameo text-slate-500/20 dark:text-slate-800/30 text-pretty  text-[15rem];
-  }
+  /* .statusCode {
+    @apply font-cameo text-slate-500/20 dark:text-slate-800/30 text-pretty  text-[35rem];
+  } */
 </style>
