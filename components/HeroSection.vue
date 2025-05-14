ef1a27f978adb304d1de1266fac98b1b1b94ec78
finabluma <oscar.francino@finabluma.com>
2025-05-14 13:08:08 +0200

    .

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -11,7 +11,23 @@
   const { gsap, ScrollTrigger } = useGsap()
 
   onMounted(() => {
-    ctx = gsap.context((self) => {}, main.value)
+    ctx = gsap.context((self) => {
+      let tl = gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: '.hero',
+            start: 'top top',
+            pin: '.hero-content',
+            scrub: true,
+            pinSpacing: false,
+          },
+        })
+        .to('.not-sidebar > *', {
+          yPercent: 10,
+          autoAlpha: 0,
+        })
+      return tl
+    }, main.value)
   })
 
   onUnmounted(() => {
@@ -20,12 +36,15 @@
 </script>
 <template>
   <div class="u-full-width hero">
-    <div class="l-center">
-      <div class="l-sidebar">
-        <div>
-          <div class="not-sidebar">
-            <div class="component component--text">
-              <slot></slot>
+    <div class="hero-content">
+      <div class="bg" role="presentation"></div>
+      <div class="l-center">
+        <div class="l-sidebar">
+          <div>
+            <div class="not-sidebar">
+              <div class="component component--text">
+                <slot></slot>
+              </div>
             </div>
           </div>
         </div>
@@ -36,33 +55,42 @@
 </template>
 <style lang="postcss">
   .hero {
-    @apply relative
-    bg-azulejos
-    bg-cover
-    lg:py-10;
+    .hero-content {
+      @apply relative;
+    }
 
-    &:before {
-      @apply content-['']
-      absolute
-      top-0
+    .bg {
+      @apply absolute
       w-full
       h-full
-      backdrop-contrast-75
-      bg-gradient-to-b
-      from-firstdark/80
-      from-40%
-      to-black
-      mix-blend-multiply
-      dark:from-firstdark/60
-      dark:via-firstdark/90
-      dark:to-firstdark
-      dark:mix-blend-darken;
+      z-10
+      bg-azulejos
+      bg-cover;
+
+      &:before {
+        @apply content-['']
+        absolute
+        top-0
+        w-full
+        h-full
+        backdrop-contrast-75
+        bg-gradient-to-b
+        from-firstdark/80
+        from-40%
+        to-black
+        mix-blend-multiply
+        dark:from-firstdark/60
+        dark:via-firstdark/90
+        dark:to-firstdark
+        dark:mix-blend-darken;
+      }
     }
   }
 
-  .hero .l-center {
+  .hero .hero-content .l-center {
     @apply relative
-    z-20;
+    z-20
+    lg:py-10;
   }
 
   .hero .l-sidebar > *,
