e29bfb73b82e647e06eb661731ad191f6f8d61a2
finabluma <oscar.francino@finabluma.com>
2025-05-14 13:17:35 +0200

    .

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -75,10 +75,10 @@
         h-full
         backdrop-contrast-75
         bg-gradient-to-b
-        from-firstdark/80
+        from-firstdark/90
         from-40%
-        to-black
-        mix-blend-multiply
+        to-firstdark
+        mix-blend-darken
         dark:from-firstdark/60
         dark:via-firstdark/90
         dark:to-firstdark
