42f00dd5be14159470b5036e6e90f13785a24f3d
finabluma <oscar.francino@finabluma.com>
2025-05-16 10:50:05 +0200

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,11 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
     head:{
       link:[
-        { rel: 'stylesheet', href: 'https://use.typekit.net/hnq7yja.css' },,
+        { rel:"icon", type:"image/png", href:"/favicon-96x96.png", sizes:"96x96" },
+        { rel:"icon", type:"image/svg+xml", href:"/favicon.svg"},
+        { rel:"shortcut icon", href:"/favicon.ico"},
+        { rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png"},
+        { rel: 'stylesheet', href: 'https://use.typekit.net/hnq7yja.css' },
       ]
     }    
   },
