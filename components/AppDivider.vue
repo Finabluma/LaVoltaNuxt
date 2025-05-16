56d4e9748953c0377c5323f059227bc694ee78d1
finabluma <oscar.francino@finabluma.com>
2025-05-16 09:09:49 +0200

    .

--- components/AppDivider.vue
+++ components/AppDivider.vue
@@ -33,7 +33,7 @@
       @apply w-full
       fill-white
       dark:fill-secondark
-      drop-shadow-[0_25px_35px_rgba(0,0,0,0.25)]
+      drop-shadow-[0_25px_35px_rgba(0,0,0,0.75)]
       dark:drop-shadow-[0_20px_35px_rgba(255,255,255,0.5)];
     }
 
