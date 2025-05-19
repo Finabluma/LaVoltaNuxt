5a736eafbc278e52964dd10291b8a52f9c94607d
Finabluma <oscar.francino@finabluma.com>
2025-05-19 13:00:16 +0200

    .

--- components/CartaMenu.vue
+++ components/CartaMenu.vue
@@ -1,104 +1,72 @@
 <script setup>
-  import { onMounted, onUnmounted } from 'vue'
-  const props = defineProps({
-    items: {
-      type: Array,
-    },
-  })
-  let main = ref(),
-    ctx = ref()
-  //GSAP
-  const { gsap, ScrollTrigger } = useGsap()
+import { onMounted, onUnmounted } from 'vue'
+const props = defineProps({
+  items: {
+    type: Array,
+  },
+})
+let cartamenu = ref(),
+  ctx = ref()
+//GSAP
+const { gsap, ScrollTrigger } = useGsap()
 
-  function tlCartaMenu() {
-    let panels = gsap.utils.toArray('.panel')
-    panels.forEach((panel, i) => {
-      ScrollTrigger.create({
-        trigger: panel,
-        start: () =>
-          panel.offsetHeight < window.innerHeight
-            ? 'top top+=10'
-            : 'bottom bottom',
-        pin: true,
-        scrub: true,
-        pinSpacing: false,
-        preventOverlaps: true,
-        fastScrollEnd: true,
-        invalidateOnRefresh: true,
-        anticipatePin: 1,
-      })
-
-      const isMedia = panel.classList.contains('media')
-      const isContent = panel.classList.contains('content')
-      const inner = panel.querySelector('.inner')
-
-      if (isMedia) {
-        const svg = inner.querySelector('svg')
-        const rect = svg.querySelector('.rect')
-        const circle = svg.querySelector('.circle')
-        const object = svg.querySelector('.object')
-
-        const tlMedia = gsap
-          .timeline({
-            scrollTrigger: {
-              trigger: panel,
-              start: 'top center',
-              scrub: true,
-              fastScrollEnd: true,
-            },
-          })
-          // .set(svg, { fillOpacity: 1, strokeOpacity: 1 })
-          .add('svg')
-          .to(circle, {
-            morphSVG: {
-              shape: rect,
-              map: 'position',
-            },
-          })
-        // .fromTo(
-        //   object,
-        //   { drawSVG: '50% 50%' },
-        //   { drawSVG: '100%' },
-        //   'svg-=0.2'
-        // )
-        // .to(object, { fillOpacity: 0.8 }, 'svg-=0.2')
-        // .to(object, { strokeOpacity: 0 }, 'svg-=0.2')
-
-        return tlMedia
-      }
-
-      // if (isContent) {
-      //   gsap.fromTo(
-      //     inner,
-      //     { y: 100, opacity: 0 },
-      //     {
-      //       y: 0,
-      //       opacity: 1,
-      //       ease: 'power2.out',
-      //       delay: 0.2, // Retraso para evitar que todas las animaciones se inicien a la vez
-      //       stagger: 0.1, // Stagger para suavizar la animación
-      //       scrollTrigger: {
-      //         trigger: panel,
-      //         start: 'top center',
-      //         scrub: true,
-      //         preventOverlaps: true, // Previene superposiciones entre animaciones de ScrollTrigger
-      //         fastScrollEnd: true,
-      //       },
-      //     }
-      //   )
-      // }
+function tlCartaMenu() {
+  let panels = gsap.utils.toArray('.panel')
+  panels.forEach((panel, i) => {
+    ScrollTrigger.create({
+      trigger: panel,
+      start: () =>
+        panel.offsetHeight < window.innerHeight
+          ? 'top top+=10'
+          : 'bottom bottom',
+      pin: true,
+      scrub: true,
+      pinSpacing: false,
+      preventOverlaps: true,
+      fastScrollEnd: true,
+      invalidateOnRefresh: true,
+      anticipatePin: 1,
     })
-  }
 
-  onMounted(() => {
-    ctx = gsap.context((self) => {
-      tlCartaMenu()
-    }, main.value)
-  })
+    const isMedia = panel.classList.contains('media')
+    const inner = panel.querySelector('.inner')
 
-  onUnmounted(() => {
-    ctx.revert()
+    if (isMedia) {
+      const svg = inner.querySelector('svg')
+      const rect = svg.querySelector('.rect')
+      const circle = svg.querySelector('.circle')
+
+      const tlMedia = gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: panel,
+            start: 'top center',
+            scrub: true,
+            fastScrollEnd: true,
+          },
+        })
+        .add('svg')
+        .to(circle, {
+          morphSVG: {
+            shape: rect,
+            map: 'position',
+          },
+        })
+
+      return tlMedia
+    }
   })
+}
+
+onMounted(() => {
+  ctx = gsap.context((self) => {
+    tlCartaMenu()
+  }, cartamenu.value)
+})
+
+onUnmounted(() => {
+  ctx.revert()
+})
 </script>
 <template>
   <div id="container">
@@ -108,7 +76,9 @@
           <div v-if="index == 0" class="svg">
             <SVGCartaCircle />
           </div>
-          <div v-if="index == 1" class="svg"><SVGMenuCircle /></div>
+          <div v-if="index == 1" class="svg">
+            <SVGMenuCircle />
+          </div>
         </div>
       </div>
       <div class="panel content">
@@ -119,20 +89,12 @@
             <p>{{ item.excerpt }}</p>
 
             <div v-if="index == 0">
-              <NuxtLink
-                to="la-carta"
-                :title="item.link.linkTarget.title"
-                class="cta"
-              >
+              <NuxtLink to="la-carta" :title="item.link.linkTarget.title" class="cta">
                 {{ item.link.title || item.link.linkTarget.title }}
               </NuxtLink>
             </div>
             <div v-if="index == 1">
-              <NuxtLink
-                to="el-menu"
-                :title="item.link.linkTarget.title"
-                class="cta"
-              >
+              <NuxtLink to="el-menu" :title="item.link.linkTarget.title" class="cta">
                 {{ item.link.title || item.link.linkTarget.title }}
               </NuxtLink>
             </div>
@@ -143,84 +105,55 @@
   </div>
 </template>
 <style lang="postcss" scoped>
-  #container {
-    @apply relative
-    w-full
-    overflow-y-hidden;
+#container {
+  @apply relative w-full overflow-y-hidden;
 
-    article {
-      @apply l-center
-      lg:flex
-      lg:items-center;
+  article {
+    @apply l-center lg:flex lg:items-center;
+  }
+
+  .panel {
+    @apply relative w-full bg-white dark:bg-secondark will-change-transform;
+
+    .inner {
+      @apply flex justify-center;
     }
+  }
 
-    .panel {
-      @apply relative
-      w-full
-    bg-white
-    dark:bg-secondark
-      will-change-transform;
-
-      .inner {
-        @apply flex
-        justify-center;
+  .media {
+    .inner {
+      .svg {
+        @apply relative z-20 w-5/12 py-10 sm:w-5/12 md:w-4/12 md:py-2 lg:w-8/12 lg:py-8;
       }
     }
+  }
 
-    .media {
-      .inner {
-        .svg {
-          @apply relative
-          z-20
-          w-5/12
-          py-10
-          sm:w-5/12
-          md:w-4/12
-          md:py-2
-          lg:w-8/12
-          lg:py-8;
+  .content {
+    @apply relative bg-white dark:bg-secondark;
+
+    .inner {
+      @apply bg-white dark:bg-secondark w-auto py-5 sm:py-10 lg:py-20;
+
+      .component.component--text {
+        @apply text-center lg:px-0 lg:text-left lg:mx-0;
+
+        >* {
+          @apply mb-clus2lev mx-auto;
         }
-      }
-    }
 
-    .content {
-      @apply relative
-        bg-white
-        dark:bg-secondark;
+        .title-block {
+          @apply mb-5 text-xl uppercase;
 
-      .inner {
-        @apply bg-white
-        dark:bg-secondark
-        w-auto
-        py-5
-        sm:py-10
-        lg:py-20;
-        .component.component--text {
-          @apply text-center
-          lg:px-0
-          lg:text-left
-          lg:mx-0;
-
-          > * {
-            @apply mb-clus2lev
-            mx-auto;
+          &:after {
+            @apply mx-auto lg:ml-0;
           }
+        }
 
-          .title-block {
-            @apply mb-5
-            text-xl
-            uppercase;
-
-            &:after {
-              @apply mx-auto
-              lg:ml-0;
-            }
-          }
-          .cta {
-            @apply mt-5;
-          }
+        .cta {
+          @apply mt-5;
         }
       }
     }
   }
+}
 </style>
