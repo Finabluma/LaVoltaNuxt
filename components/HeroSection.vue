6c945ae4a3ed56853697a635106921fbd5554489
Finabluma <oscar.francino@finabluma.com>
2025-05-19 14:31:35 +0200

    .

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -78,11 +78,11 @@ onUnmounted(() => {
       <div class="bg" role="presentation"></div>
       <div class="l-center">
         <div class="l-sidebar">
-            <div class="not-sidebar">
-              <div class="component--text">
-                <slot></slot>
-              </div>
+          <div class="not-sidebar">
+            <div class="component--text">
+              <slot></slot>
             </div>
+          </div>
         </div>
       </div>
     </div>
@@ -90,77 +90,47 @@ onUnmounted(() => {
   </div>
 </template>
 <style lang="postcss">
-  .hero {
-    .hero-content {
-      @apply relative;
-    }
+.hero {
+  .hero-content {
+    @apply relative;
+  }
 
-    .bg {
-      @apply absolute
-      w-full
-      h-full
-      z-10
-      bg-azulejos2
-      dark:bg-azulejos
-      bg-cover;
+  .bg {
+    @apply absolute w-full h-full z-10 bg-azulejos2 dark:bg-azulejos bg-cover;
 
-      &:before {
-        @apply content-['']
-        absolute
-        top-0
-        w-full
-        h-full
-        bg-gradient-to-b
-        from-white/90
-        via-white/90
-        to-white
-        mix-blend-color-dodge
-        dark:bg-gradient-to-b
-        dark:from-secondark
-        dark:to-black
-        dark:backdrop-brightness-90
-        dark:mix-blend-multiply;
-      }
+    &:before {
+      @apply content-[''] absolute top-0 w-full h-full bg-gradient-to-b from-white/90 via-white/90 to-white mix-blend-color-dodge dark:bg-gradient-to-b dark:from-secondark dark:to-black dark:backdrop-brightness-90 dark:mix-blend-multiply;
     }
   }
+}
 
-  .hero .hero-content .l-center {
-    @apply relative
-    z-20
-    lg:py-10;
-  }
+.hero .hero-content .l-center {
+  @apply relative z-20 lg:py-10;
+}
 
-  .hero .l-sidebar > *,
-  .hero .l-sidebar > * > * {
-    @apply m-0;
-  }
+.hero .l-sidebar>*,
+.hero .l-sidebar>*>* {
+  @apply m-0;
+}
 
-  .hero .l-sidebar .sidebar {
-    @apply basis-96;
-  }
+.hero .l-sidebar .sidebar {
+  @apply basis-96;
+}
 
-  .hero .l-sidebar .not-sidebar {
-    @apply dark:text-secondark
-    pt-24
-    pb-16
-    mx-auto
-    lg:max-w-5xl
-    lg:px-10;
-  }
+.hero .l-sidebar .not-sidebar {
+  @apply dark:text-secondark pt-28 sm:pb-16 lg:pt-20 pb-12 mx-auto lg:max-w-5xl lg:px-10;
+}
 
-  .hero .lead {
-    @apply mb-6
-    mt-0
-    font-semibold;
-  }
+.hero .lead {
+  @apply mb-6 mt-0 font-semibold;
+}
 
-  .hero h1,
-  .hero h2 {
-    @apply mb-clus3lev;
-  }
+.hero h1,
+.hero h2 {
+  @apply mb-clus3lev;
+}
 
-  .hero + * {
-    @apply pt-8
-    lg:pt-14;
-  }
+.hero+* {
+  @apply pt-8 lg:pt-14;
+}
 </style>
