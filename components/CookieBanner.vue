c5a99dcebca5d6e963bf2fcb08eedf74dcaa04ce
Finabluma <oscar.francino@finabluma.com>
2025-05-23 19:12:18 +0200

    .

--- components/CookieBanner.vue
+++ components/CookieBanner.vue
@@ -1,91 +1,30 @@
 <script setup>
-  import { defineEmits } from 'vue'
+import { defineEmits } from 'vue'
 
-  const emit = defineEmits(['accept', 'reject'])
+const emit = defineEmits(['accept', 'reject'])
 
-  function onAccept() {
-    emit('accept')
-  }
+function onAccept() {
+  emit('accept')
+}
 
-  function onReject() {
-    emit('reject')
-  }
+function onReject() {
+  emit('reject')
+}
 </script>
 
 <template>
-  <div class="cookies-banner">
-    <div class="inner">
-      <p>
+  <div
+    class="fixed w-full bottom-0 z-40 bg-white dark:bg-secondark flex justify-center items-center drop-shadow-cookies">
+    <div
+      class="max-w-5xl mx-auto flex flex-col items-center py-2 md:flex-row md:justify-between md:items-center md:py-4">
+      <p class="text-sm text-center md:text-left">
         Usamos <b>Google Analytics</b> para analizar el uso de nuestro sitio.
         <b>¿Aceptas el uso de cookies para estos fines?</b>
       </p>
-      <div class="buttons">
-        <button @click="onAccept" >Aceptar</button>
-        <button @click="onReject">Rechazar</button>
+      <div class="flex items-center p-2 md:p-0 md:ml-4">
+        <button @click="onAccept" class="cookies-buttons">Aceptar</button>
+        <button @click="onReject" class="cookies-buttons">Rechazar</button>
       </div>
     </div>
   </div>
 </template>
-
-<style lang="postcss" scoped>
-  .cookies-banner {
-    @apply w-full
-    fixed
-    bottom-0
-    z-40
-    bg-white
-    dark:bg-secondark
-    flex
-    justify-center
-    items-center
-    drop-shadow-[0_2px_8px_rgba(0,0,0,0.25)];
-
-    .inner {
-      @apply l-center
-      flex
-      flex-col
-      py-2
-      items-center
-      md:flex-row
-      md:justify-between
-      md:items-center
-      md:py-4;
-
-      p {
-        @apply text-sm
-        text-center
-        md:text-left;
-      }
-
-      .buttons {
-        @apply flex
-        items-center
-        p-2
-        md:p-0
-        md:ml-4;
-
-        button {
-          @apply first:mr-2
-            inline-flex
-            font-semibold
-            py-2
-            px-4
-            rounded-lg
-            outline
-            outline-2
-            -outline-offset-1
-            outline-firstlight
-            dark:outline-firstdark
-            bg-firstlight
-            text-white
-            dark:bg-firstdark    
-            dark:text-secondark
-            hover:bg-white
-            hover:text-firstlight
-            dark:hover:bg-secondark    
-            dark:hover:text-firstdark;
-        }
-      }
-    }
-  }
-</style>
--- components/CookieToggle.vue
+++ components/CookieToggle.vue
@@ -1,32 +1,17 @@
 <script setup>
-  import { defineEmits } from 'vue'
+import { defineEmits } from 'vue'
 
-  const emit = defineEmits(['toggle'])
+const emit = defineEmits(['toggle'])
 
-  function onClick() {
-    emit('toggle')
-  }
+function onClick() {
+  emit('toggle')
+}
 </script>
 
 <template>
-  <button
-    @click="onClick"
-    class="cookie-toggle"
-    title="Cambiar preferencias de cookies"
-  >
+  <button @click="onClick"
+    class="fixed bottom-3 right-3 bg-white/80 dark:bg-secondark/80 text-xl p-2 rounded-full drop-shadow-md"
+    title="Cambiar preferencias de cookies">
     <SVGCookie />
   </button>
 </template>
-<style lang="postcss" scoped>
-  .cookie-toggle {
-    @apply fixed
-    bottom-3 
-    right-3
-    bg-white/80
-    dark:bg-secondark/80
-    text-xl
-    p-2
-    rounded-full 
-    shadow-md;
-  }
-</style>
