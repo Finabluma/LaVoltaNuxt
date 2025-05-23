abe93273ec7ba3cc3922425f347ab5d7dfc4e8c8
Finabluma <oscar.francino@finabluma.com>
2025-05-23 19:02:52 +0200

    .

--- components/AppModal.vue
+++ components/AppModal.vue
@@ -1,110 +1,48 @@
 <script setup>
-  const { gsap } = useGsap()
-  const props = defineProps({
-    budskap: {
-      type: Object,
-    },
-  })
+const { gsap } = useGsap()
+const props = defineProps({
+  budskap: {
+    type: Object,
+  },
+})
 
-  let main = ref(),
-    ctx = ref()
+let main = ref(),
+  ctx = ref()
 
-  function banner() {
-    let q = gsap.utils.selector('.modal-overlay')
-    gsap
-      .timeline({
-        delay: 0.5,
-      })
-      .set('.modal-overlay', { visibility: 'visible' })
-      .from(q('.wrapper'), { autoAlpha: 0 })
-  }
+function banner() {
+  let q = gsap.utils.selector('.modal-overlay')
+  gsap
+    .timeline({
+      delay: 0.5,
+    })
+    .set('.modal-overlay', { visibility: 'visible' })
+    .from(q('.wrapper'), { autoAlpha: 0 })
+}
 
-  onMounted(() => {
-    ctx = gsap.context((self) => {
-      banner()
-    }, main.value)
-  })
+onMounted(() => {
+  ctx = gsap.context((self) => {
+    banner()
+  }, main.value)
+})
 
-  onUnmounted(() => {
-    ctx.revert()
-  })
+onUnmounted(() => {
+  ctx.revert()
+})
 </script>
 <template>
-  <div v-if="budskap.bannerUrgent" class="modal-overlay">
-    <div class="wrapper">
-      <div class="close" @click="$emit('close-modal')">
-        <Close />
+  <div v-if="budskap.bannerUrgent" class="fixed top-24 z-50 w-full invisible">
+    <div class="relative w-10/12 mx-auto bg-firstlight dark:bg-firstdark">
+      <div class="absolute top-2 right-2 opacity-50 w-10 block" @click="$emit('close-modal')">
+        <Close class="p-1 fill-white" />
       </div>
-      <div class="modal l-box l-box--no-border">
-        <InfoModal />
-        <div class="content">
-          <!-- <h6>{{ budskap.bannerUrgent.urgentContent.title }}</h6> -->
-          <p>{{ budskap.bannerUrgent.urgentContent.banner }}</p>
+      <div
+        class="max-w-5xl mx-auto l-box l-box--no-border flex flex-col justify-center items-center sm:flex-row sm:justify-start">
+        <InfoModal class="w-10 mb-5 sm:mb-0 sm:mr-5" />
+        <div class="sm:w-10/12">
+          <p class="font-coordinates font-semibold text-center text-sm text-white dark:text-secondark sm:text-left">{{
+            budskap.bannerUrgent.urgentContent.banner }}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
-<style lang="postcss" scoped>
-  .modal-overlay {
-    @apply fixed
-    top-24
-    z-50
-    w-full
-    invisible
-    md:top-24;
-
-    .wrapper {
-      @apply relative
-      w-10/12
-      mx-auto
-      bg-firstlight
-      dark:bg-firstdark;
-
-      .close {
-        @apply absolute
-        top-2
-        right-2
-        opacity-50
-        cursor-pointer
-        w-10
-        block;
-
-        svg {
-          @apply p-1
-          fill-white;
-        }
-      }
-
-      .modal {
-        @apply l-center
-        flex
-        flex-col
-        justify-center
-        items-center
-        sm:flex-row
-        sm:justify-start;
-
-        svg {
-          @apply w-10
-          mb-clus2lev
-          sm:mr-clus3lev
-          sm:mb-0;
-        }
-
-        .content {
-          @apply sm:w-10/12;
-
-          p {
-            @apply font-coordinates
-            font-semibold
-            text-center
-            text-sm
-            text-white           dark:text-secondark
-            sm:text-left;
-          }
-        }
-      }
-    }
-  }
-</style>
