124a77be2e4def91585684500d967dbd54cc8065
Finabluma <oscar.francino@finabluma.com>
2025-05-25 16:56:51 +0200

    .

--- components/CartaMenu.vue
+++ components/CartaMenu.vue
@@ -11,94 +11,80 @@ const { gsap, ScrollTrigger } = useGsap()
 const cartamenu = ref(null)
 let ctx, mm
 
-// onMounted(() => {
-//   ctx = gsap.context(() => {
-//     mm = gsap.matchMedia()
+function PanelsTl() {
+  const panels = gsap.utils.toArray('.panel')
+  panels.forEach((panel) => {
+    ScrollTrigger.create({
+      trigger: panel,
+      start:
+        panel.offsetHeight < window.innerHeight
+          ? 'top top+=10'
+          : 'bottom bottom',
+      pin: true, // aquí controlamos el pin según el dispositivo
+      pinSpacing: false,
+      scrub: true,
+      fastScrollEnd: true,
+      anticipatePin: 1,
+      invalidateOnRefresh: true,
+    })
 
-//     mm.add(
-//       {
-//         isDesktop: '(min-width: 1024px)',
-//         isMobile: '(max-width: 1023px)',
-//       },
-//       (context) => {
-//         const { isDesktop, isMobile } = context.conditions
-//         const panels = gsap.utils.toArray('.panel')
+    const media = panel.querySelector('.media')
 
-//         panels.forEach((panel) => {
-//           ScrollTrigger.create({
-//             trigger: panel,
-//             start:
-//               panel.offsetHeight < window.innerHeight
-//                 ? 'top top+=10'
-//                 : 'bottom bottom',
-//             pin: isDesktop, // aquí controlamos el pin según el dispositivo
-//             pinSpacing: false,
-//             scrub: true,
-//             fastScrollEnd: true,
-//             anticipatePin: 1,
-//             invalidateOnRefresh: true,
-//           })
+    const content = panel.querySelector('.content')
 
-//           // Animación SVG si .media
-//           if (panel.classList.contains('media')) {
-//             const inner = panel.querySelector('.inner')
-//             if (!inner) return
+    if (media) {
+      const rect = media.querySelector('.rectSVG')
+      const circle = media.querySelector('.circleSVG')
+      const obj = media.querySelector('.objectSVG')
 
-//             const svg = inner.querySelector('svg')
-//             if (!svg) return
+      let tl = gsap.timeline().to(circle, {
+        morphSVG: { shape: rect, map: 'position' },
+      })
 
-//             const rect = svg.querySelector('.rectSVG')
-//             const circle = svg.querySelector('.circleSVG')
-//             if (!rect || !circle) return
+      ScrollTrigger.create({
+        trigger: panel,
+        start: 'center center',
+        scrub: true,
+        animation: tl
+      })
+    }
 
-//             gsap.timeline({
-//               scrollTrigger: {
-//                 trigger: panel,
-//                 start: 'top center',
-//                 scrub: true,
-//                 fastScrollEnd: true,
-//               },
-//             })
-//               .to(circle, {
-//                 morphSVG: { shape: rect, map: 'position' },
-//               })
-//           }
+    if (content) {
+      const text = panel.querySelector('.component--text')
+      let tl = gsap.timeline().to(text, {
+        yPercent: 10,
+      }).to(text, {
+        autoAlpha: 0
+      })
+      ScrollTrigger.create({
+        trigger: panel,
+        start: 'center center',
+        scrub: true,
+        animation: tl,
+      })
 
-//           // Animación de entrada suave solo en móvil para panel.content
-//           if (isMobile && panel.classList.contains('content')) {
-//             gsap.fromTo(
-//               panel,
-//               { opacity: 0, y: 30 },
-//               {
-//                 opacity: 1,
-//                 y: 0,
-//                 duration: 0.8,
-//                 ease: 'power2.out',
-//                 scrollTrigger: {
-//                   trigger: panel,
-//                   start: 'top 90%',
-//                   toggleActions: 'play reverse play reverse',
-//                 },
-//               }
-//             )
-//           }
-//         })
-//       }
-//     )
-//   }, cartamenu.value)
+    }
+  })
+}
 
-// })
 
-// onUnmounted(() => {
-//   if (mm) mm.revert()
-//   if (ctx) ctx.revert()
-// })
+onMounted(() => {
+  ctx = gsap.context(() => {
+    PanelsTl()
+  }, cartamenu.value)
+})
+
+onUnmounted(() => {
+  ctx.revert()
+})
+
+
 </script>
 <template>
   <div class="carta-menu" ref="cartamenu">
     <article v-for="(item, index) in items" :key="item._key">
-      <div class="panel">
-        <div>
+      <div class="panel ">
+        <div class="media">
           <div v-if="index == 0">
             <SVGCartaCircle />
           </div>
@@ -107,9 +93,9 @@ let ctx, mm
           </div>
         </div>
       </div>
-      <div class="panel">
-        <div class="text-center md:text-left">
-          <div class="component--text">
+      <div class="panel ">
+        <div class="content w-auto text-center md:text-left">
+          <div class="component--text py-5">
             <div>
               <h2>{{ item.heading }}</h2>
               <p class="lead">{{ item.tagline }}</p>
