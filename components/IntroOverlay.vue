d65bfa28b9690f1e48b3af75d576ad8fda09dcf0
Finabluma <oscar.francino@finabluma.com>
2025-05-26 12:13:28 +0200

    intro

--- components/IntroOverlay.vue
+++ components/IntroOverlay.vue
@@ -22,7 +22,7 @@ onMounted(() => {
         .set('.intro-text', { autoAlpha: 1 })
         .from(split.chars, {
             duration: 0.5,
-            yPercent: "random([-10, 100])",
+            yPercent: "random([-100, 100])",
             rotation: "random(-60, 60)",
             autoAlpha: 0,
             stagger: {
@@ -43,7 +43,7 @@ onMounted(() => {
 </script>
 <template>
     <div v-if="show"
-        class="intro-overlay fixed top-0 left-0 z-[100] w-full h-full bg-firstlight text-white/80 dark:bg-secondark dark:text-firstdark flex justify-center items-center overflow-hidden"
+        class="intro-overlay fixed top-0 left-0 z-[100] w-full h-full bg-white text-firstlight dark:bg-secondark dark:text-firstdark flex justify-center items-center overflow-hidden"
         ref="overlay">
         <div class="intro-text font-bold text-fluid-5xl opacity-0">Bienvenid@</div>
     </div>
