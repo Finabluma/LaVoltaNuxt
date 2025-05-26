3578fa2f18c1c6fe7f74fadf1a2f50f790da44b7
Finabluma <oscar.francino@finabluma.com>
2025-05-26 14:13:55 +0200

    ScrollSmooth speed

--- app.vue
+++ app.vue
@@ -124,7 +124,7 @@ onMounted(() => {
       content: '#smooth-content',
       smooth: 1.5,
       effects: true,
-      smoothTouch: 0.1,
+      smoothTouch: 0.5,
       speed: 2,
     })
     ScrollTrigger.refresh()
