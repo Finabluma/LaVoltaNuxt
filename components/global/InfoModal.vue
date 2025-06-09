51afd80efb2e28f7b2d10c97c101d9376eabd420
Finabluma <oscar.francino@finabluma.com>
2025-06-09 18:50:19 +0200

    .

--- components/global/InfoModal.vue
+++ components/global/InfoModal.vue
@@ -1,7 +1,16 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <path fill-rule="evenodd"
-      d="M9.73 3.993a2.75 2.75 0 0 1 4.54 0l.432.632a76 76 0 0 1 6.944 12.563l.09.208a2.51 2.51 0 0 1-2.024 3.497a69.4 69.4 0 0 1-15.424 0a2.51 2.51 0 0 1-2.024-3.497l.09-.208A76 76 0 0 1 9.298 4.625zM13 9a1 1 0 1 1-2 0a1 1 0 0 1 2 0m-1 2.75a.75.75 0 0 1 .75.75v5a.75.75 0 1 1-1.5 0v-5a.75.75 0 0 1 .75-.75"
-      clip-rule="evenodd" />
+  <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 512 512">
+    <circle cx="256" cy="256" r="144" stroke-miterlimit="10" stroke-width="24" class="fill-white dark:fill-secondark" />
+    <path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="6"
+      d="M256 200v-48m108 104h-48m-116 0h-48m180-68l-24 24m-104 0l-24-24m128 112l24 24m-152 0l24-24"
+      class="stroke-firstlight dark:stroke-firstdark" />
+    <circle cx="256" cy="256" r="24" class="fill-white dark:fill-secondark" />
+    <path fill="none" stroke-linecap="round" stroke-miterlimit="10" stroke-width="16" d="M256 284V164"
+      class="stroke-firstlight dark:stroke-firstdark">
+      <animateTransform attributeName="transform" calcMode="spline" dur="6s"
+        keySplines=".42, 0, .58, 1; .42, 0, .58, 1; .42, 0, .58, 1; .42, 0, .58, 1; .42, 0, .58, 1; .42, 0, .58, 1"
+        keyTimes="0; .17; .25; .42; .5; .67; 1" repeatCount="indefinite" type="rotate"
+        values="-54 256 256; -15 256 256; -36 256 256; 36 256 256; 10 256 256; 115 256 256; -54 256 256" />
+    </path>
   </svg>
 </template>
