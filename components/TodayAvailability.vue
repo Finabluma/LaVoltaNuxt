7aaf1d53b976699c49a917e3fdf579deaccdee48
Finabluma <oscar.francino@finabluma.com>
2025-06-27 11:27:14 +0200

    horarios

--- components/TodayAvailability.vue
+++ components/TodayAvailability.vue
@@ -19,7 +19,7 @@ const dag = computed(() => {
   return dia.value[0].availableTimes
 })
 function formatHour(item) {
-  return item.slice(0, -2)
+  return item.slice(0)
 }
 </script>
 <template>
@@ -28,30 +28,31 @@ function formatHour(item) {
     <div class="container">
       <div v-if="dag.length === 0" class="state">
         <div>
-          <Reloj class="mr-1" /> <span>Cerrado</span>
+          <Reloj class="mr-1" /><span>HOY CERRAMOS</span>
         </div>
       </div>
       <div v-if="dag.length === 1" class="state">
         <div>
-          <Reloj class="mr-1" />
-          <span class="sm:mr-0.5">Hoy abrimos</span>
+          <span>Cocina abierta</span>
+          <Reloj class="mr-0.5" />
         </div>
         <div v-for="hours in dag" :key="hours.key" class="franjas">
           <div>
-            <span>{{ formatHour(hours.from) }}</span>
-            <span> -{{ formatHour(hours.to) }}</span>pm
+            <span>{{ hours.from }}</span>
+            <span>-{{ formatHour(hours.to) }}</span>
           </div>
         </div>
       </div>
       <div v-if="dag.length === 2" class="state">
         <div>
-          <Reloj class="mr-1" />
-          <span class="sm:mr-2">Abierto</span>
+
+          <span class="sm:mr-0.5">Cocina abierta</span>
+          <Reloj />
         </div>
         <div class="franjas">
           <div v-for="hours in dag" :key="hours.key" class="franja">
             <span>{{ formatHour(hours.from) }}</span>
-            <span> -{{ formatHour(hours.to) }}</span>pm
+            <span>-{{ formatHour(hours.to) }}</span>
           </div>
         </div>
       </div>
